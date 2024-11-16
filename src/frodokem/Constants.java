e8483cff932538b3f22415a71c0073d05ab61541
DianaBeqiri <74824755+DianaBeqiri@users.noreply.github.com>
2024-11-16 21:31:30 +0100

    Create Constants.java
    necessary constants

---
+++ src/frodokem/Constants.java
@@ -0,0 +1,8 @@
+package frodokem;
+
+public class Constants {
+    public static final int Q = 32768; // Modulus for FrodoKEM-640
+    public static final int MATRIX_SIZE = 640; // Actual matrix size for FrodoKEM-640
+    public static final double SIGMA = 2.8; // Standard deviation for discrete Gaussian noise
+
+}
